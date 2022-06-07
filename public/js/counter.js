f8212326b21658ac7aa354fc09ff9fee3f72b1c4
Hakan Yaman <hakanyaman5249@gmail.com>
2022-06-07 13:59:45 +0300

    first commit

---
+++ public/js/counter.js
@@ -0,0 +1,18 @@
+const counters = document.querySelectorAll(".counter");
+const speed = 300;
+
+counters.forEach((counter) => {
+  const updateCount = () => {
+    const target = +counter.getAttribute("data-target");
+    const count = +counter.innerText;
+    const inc = target / speed;
+    console.log(inc);
+    if (count < target) {
+      counter.innerText = Math.ceil(count + inc);
+      setTimeout(updateCount, 3);
+    } else {
+      count.innerText = target;
+    }
+  };
+  updateCount();
+});
